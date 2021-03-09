48e385f97f8174565bcff0612c0d6223a07f936b
JohannaMathieu <johannamathieu@yahoo.de>
2021-03-09 10:17:30 +0100

    play with variables

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
+//variables
+var userName = "Lucy";
+
+const birthPlace = "Fritzlar";
+
+
+//some stuff to put in the console
 console.log("Hello, World");
+console.info(2, 3, 4);
+console.warn(userName);
+console.log(birthPlace);
+
+//some changing a variable
+let myBudget = 0;
+
+myBudget++;
+
+console.log(myBudget); // Should print 1
+
+
+//add text to string
+let hello = "Hello";
+
+hello += ", World!";
+
+console.log(hello);
+
+// "Hello, World!"
+
+
+myBudget += 2;
+
+console.log(myBudget); // Should print 3
+
+
+myBudget = myBudget + 1;
+
+console.log(myBudget); // Should print 4
+
+
+myBudget--;
+
+console.log(myBudget); // Should print 3
+
+
+myBudget -= 2;
+
+console.log(myBudget); // Should print 1
+
+
+myBudget = myBudget - 1;
+
+console.log(myBudget); // Should print 0
