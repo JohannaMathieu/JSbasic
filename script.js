90c554a3b42c5024602ced1bb145de48a6c912f0
JohannaMathieu <johannamathieu@yahoo.de>
2021-03-10 11:20:17 +0100

    changed alot of pictures with js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//getting my image and changing the source
-const myImg = document.querySelector(".my-img");
-myImg.src = "https://cdn.pixabay.com/photo/2021/01/30/12/19/couple-5963678_1280.png";
+// //getting my image and changing the source
+// const myImg = document.querySelector(".my-img");
+// myImg.src = "https://cdn.pixabay.com/photo/2021/01/30/12/19/couple-5963678_1280.png";
 
 //changing the h1
 
@@ -10,3 +10,18 @@ headline.innerHTML = "Welcome everyone!"
 let user = window.prompt("Please enter your name");
 
 headline.innerHTML = `Welcome, ${user}`;
+
+//changing an array of pictures
+const picArray = [
+    "https://placedog.net/500/280",
+    "https://placedog.net/500/205",
+    "https://placedog.net/500/230",
+    "https://placedog.net/500/284",
+    "https://placedog.net/500/301"
+];
+
+const myImages = document.querySelectorAll("img");
+
+for (i = 0; i < picArray.length; i++) {
+    myImages[i].src = picArray[i];
+}
