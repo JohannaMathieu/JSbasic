a98b7286f49c431b54370ba47b09dc368abd4697
JohannaMathieu <johannamathieu@yahoo.de>
2021-03-10 11:08:56 +0100

    changed the title with user input

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 //getting my image and changing the source
 const myImg = document.querySelector(".my-img");
 myImg.src = "https://cdn.pixabay.com/photo/2021/01/30/12/19/couple-5963678_1280.png";
+
+//changing the h1
+
+const headline = document.querySelector(".headline");
+headline.innerHTML = "Welcome everyone!"
+
+let user = window.prompt("Please enter your name");
+
+headline.innerHTML = `Welcome, ${user}`;
